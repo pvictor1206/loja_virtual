72c92f652e4bdc8275a4731f8df7333aaf99a4a1
Paulo Victor Santos Magalhães <72519926+pvictor1206@users.noreply.github.com>
2021-05-19 19:09:13 -0300

    commit home

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:loja_virtual/screens/home.dart';
 
 class AppWidget extends StatelessWidget {
   // This widget is the root of your application.
@@ -6,7 +7,12 @@ class AppWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Loja Virtual',
-      home: Home(), //Tela principal
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: Color.fromARGB(255, 4, 125, 141),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Container(), //Tela principal
     );
   }
 }
